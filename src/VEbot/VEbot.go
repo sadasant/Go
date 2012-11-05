e1c3920d0f9100d982b9a9785194fb54b38a12f0
Daniel Rodríguez <sadasant@gmail.com>
2012-11-04 21:31:34 -0430

    Now listening and responding to messages :)

--- src/VEbot/VEbot.go
+++ src/VEbot/VEbot.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	irc "github.com/fluffle/goirc/client"
+	"strings"
 	"time"
 )
 
@@ -13,7 +14,9 @@ func main() {
 	flag.Parse()
 	readConfig(flag.Arg(0)) // config.go
 	client := irc.SimpleClient(config.Nick)
+	client.SSL = true
 	client.AddHandler("connected", connected)
+	client.AddHandler("privmsg", message)
 	client.AddHandler("disconnected", disconnected)
 	quit := make(chan bool)
 	err := client.Connect(config.Server)
@@ -21,6 +24,22 @@ func main() {
 		fmt.Printf("Connection Error: %s\n", err)
 	}
 	<-quit
+}
+
+func message(conn *irc.Conn, line *irc.Line) {
+	fmt.Printf("%s\n", line.Args)
+	msgs := strings.Split(line.Args[1], ":")
+	if msgs[0] == config.Nick {
+		// Improve this with a JSON file full of messages and responses
+		switch {
+		case strings.LastIndex(msgs[1], "hola") >= 0:
+			conn.Privmsg(config.Channel, line.Nick+": Hola!")
+		case strings.LastIndex(msgs[1], "o/") >= 0:
+			conn.Privmsg(config.Channel, line.Nick+": \\o")
+		case strings.LastIndex(msgs[1], "hello") >= 0:
+			conn.Privmsg(config.Channel, line.Nick+": Hi!")
+		}
+	}
 }
 
 func connected(conn *irc.Conn, line *irc.Line) {
@@ -41,7 +60,7 @@ func constantlyReadTwitter(conn *irc.Conn) {
 	}
 	for _, tweet := range twitter.Results {
 		if tweet.New {
-			conn.Privmsg(config.Channel, "@" + tweet.From_user + ": \"" + tweet.Text + "\"")
+			conn.Privmsg(config.Channel, "@"+tweet.From_user+": \""+tweet.Text+"\"")
 			conn.Privmsg(config.Channel, tweet.URL)
 		}
 	}
